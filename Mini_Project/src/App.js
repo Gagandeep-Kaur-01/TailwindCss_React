07aac4f41882aea8df25b62e350f42809d3408f8
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-05-23 18:25:44 +0530

    added route for 'Home' page

--- Mini_Project/src/App.js
+++ Mini_Project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import Home from './container/pages/Home';
 import SignIn from './container/pages/SignIn';
 
 export default function App() {
@@ -9,6 +9,10 @@ export default function App() {
   return(
     <BrowserRouter>
       <Switch>
+
+      <Route exact path="/">
+          <Home />
+        </Route>
 
         <Route path="/signin">
           <SignIn />
---
+++ Mini_Project/src/container/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+        Hii
+
+    
+
+    </div>
+  );
+}
+
+export default Home;
