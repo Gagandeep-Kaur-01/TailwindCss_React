f386c55c822ef52cb92b38c24bcf6f5d0e3a9e1e
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-05-30 19:03:04 +0530

    created and inserted routes

--- Mini_Project/src/App.js
+++ Mini_Project/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './container/pages/Home';
+import About from './container/pages/About';
+import Features from './container/pages/Features';
+import Projects from './container/pages/Projects';
+import Contact from './container/pages/Contact';
 import SignIn from './container/pages/SignIn';
+import SignUp from './container/pages/SignUp';
 
 export default function App() {
 
@@ -11,12 +16,32 @@ export default function App() {
       <Switch>
 
       <Route exact path="/">
-          <Home />
-        </Route>
+        <Home />
+      </Route>
 
-        <Route path="/signin">
-          <SignIn />
-        </Route>
+      <Route exact path="/about">
+        <About />
+      </Route>  
+
+      <Route exact path="/features">
+        <Features />
+      </Route>
+
+      <Route exact path="/projects">
+        <Projects />
+      </Route>
+
+      <Route exact path="/contact">
+        <Contact />
+      </Route>
+
+      <Route path="/signin">
+        <SignIn />
+      </Route>
+
+      <Route exact path="/signup">
+        <SignUp />
+      </Route>
 
       </Switch>
     </BrowserRouter>
--- Mini_Project/src/container/partials/Header.js
+++ Mini_Project/src/container/partials/Header.js
@@ -18,22 +18,22 @@ function Header(IsBgGray, IsTextWhite) {
             <ul className="flex flex-grow justify-end flex-wrap items-center"> 
                
               <li>
-                <Link to="/" className={`hover:text-gray-200 px-6 py-2 flex items-center transition duration-150 ease-in-out `}>
+                <Link to="/about" className={`hover:text-gray-200 px-6 py-2 flex items-center transition duration-150 ease-in-out `}>
                   About
                 </Link>
               </li>             
               <li>
-                <Link to="/" className="hover:text-gray-200 px-6 py-2 flex items-center transition duration-150 ease-in-out">
+                <Link to="/features" className="hover:text-gray-200 px-6 py-2 flex items-center transition duration-150 ease-in-out">
                   Features
                 </Link>
               </li>
               <li>
-                <Link to="/" className="hover:text-gray-200 px-6 py-2 flex items-center transition duration-150 ease-in-out">
+                <Link to="/projects" className="hover:text-gray-200 px-6 py-2 flex items-center transition duration-150 ease-in-out">
                   Projects
                 </Link>
               </li>
               <li>
-                <Link to="/" className="hover:text-gray-200 px-6 py-2 flex items-center transition duration-150 ease-in-out">
+                <Link to="/contact" className="hover:text-gray-200 px-6 py-2 flex items-center transition duration-150 ease-in-out">
                   Contact
                 </Link>
                </li>
@@ -47,7 +47,9 @@ function Header(IsBgGray, IsTextWhite) {
                 </li>
                 <li>
                 <div className="pr-30">
-                <Link to="/signup" className="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign Up</Link>
+                <Link to="/signup" className="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  Sign Up
+                </Link>
                 </div> 
               </li>
 
