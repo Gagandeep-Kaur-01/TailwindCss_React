4769cc531cc5c033b5bf1baf992f95f8c289c74e
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-05-30 19:05:12 +0530

    added comment

--- Mini_Project/src/container/pages/Home.js
+++ Mini_Project/src/container/pages/Home.js
@@ -5,7 +5,8 @@ function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
 
-        <Header IsBgGray={true} IsTextWhite={true}/>
+      {/* Header */}
+      <Header IsBgGray={true} IsTextWhite={true}/>
 
     
 
