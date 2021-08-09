14218c2c5ddc36d185edfac005e59e88be39c14c
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-08-09 17:26:38 +0530

    created nav and calling About page

---
+++ Mini_Project/src/container/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../partials/Header';
+
+export default function About() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+
+      {/* Header */}
+      <Header IsBgLight={true} IsTextBlack={true} />
+
+    
+
+    </div>
+  );
+}
--- Mini_Project/src/container/partials/Header.js
+++ Mini_Project/src/container/partials/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Logo from '../../images/Project_logo.svg'
+
 
 function Header({IsBgGray, IsTextWhite}) {
     return (
@@ -9,6 +11,23 @@ function Header({IsBgGray, IsTextWhite}) {
 
                     {/* Logo */}
                     <img src={Logo} height="20%" width="10%"/>
+
+                    {/* Navigation */}
+                    <nav className="hidden md:flex md:flex-grow">
+
+                        <ul className="flex flex-wrap items-center flex-grow">
+                            <li>
+                                <Link to="/about" className={`hover:bg-gray-200 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${IsBgGray && "bg-gray-900"} ${IsTextWhite && "text-white"}`}>
+                                    About
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/features" className={`hover:bg-gray-200 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${IsBgGray && "bg-gray-900"} ${IsTextWhite && "text-white"}`}>
+                                    Features
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
 
                 </div>
 
