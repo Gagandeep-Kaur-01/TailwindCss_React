36da287f029f2414edda91c662452514d77838cd
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-08-09 18:03:20 +0530

    updated bg color and logo

--- Mini_Project/src/container/pages/Home.js
+++ Mini_Project/src/container/pages/Home.js
@@ -3,7 +3,7 @@ import Header from '../partials/Header';
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
+    <div className="flex flex-col min-h-screen overflow-hidden bg-gray-900">
 
       {/* Header */}
       <Header IsBgGray={true} IsTextWhite={true}/>
