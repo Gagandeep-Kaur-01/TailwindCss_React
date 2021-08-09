b7bbbadf4b29f65dfa8764e2e9397cb24afc5db6
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-08-09 18:40:42 +0530

    linked to home page

--- Mini_Project/src/container/partials/Header.js
+++ Mini_Project/src/container/partials/Header.js
@@ -6,11 +6,14 @@ import Logo from '../../images/Project_logo.svg'
 function Header({IsBgGray, IsTextWhite, NavBgLight, DarkText, NavBgDark, LightText }) {
     return (
         <header className="absolute w-full">
-            <div className="px-4 max-w-prose sm:mx-12 sm:px-6">
+            <div className="px-4 max-w-prose sm:mx-12 sm:px-6 py-6">
                 <div className="container relative flex items-center justify-between h-20 mx-auto">
 
                     {/* Logo */}
-                    <img src={Logo} height="20%" width="10%"/>
+                    <Link to="/">
+                        <img src={Logo} height="20%" width="20%"/>
+                    </Link>
+                    
 
                     {/* Navigation */}
                     <nav className="hidden md:flex md:flex-grow">
