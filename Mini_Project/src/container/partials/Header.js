e81a1920c13798ce5255288d5ac685f5c8910ce6
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-09-11 15:45:38 +0530

    added options for nav bar

--- Mini_Project/src/container/partials/Header.js
+++ Mini_Project/src/container/partials/Header.js
@@ -7,7 +7,7 @@ function Header({IsBgGray, IsTextWhite, NavBgLight, DarkText, NavBgDark, LightTe
     return (
         <header className="absolute w-full">
             <div className="px-4 max-w-prose sm:mx-12 sm:px-6 py-6">
-                <div className="container relative flex items-center justify-between h-20 mx-auto">
+                <div className="container relative flex items-center justify-between h-20">
 
                     {/* Logo */}
                     <Link to="/">
@@ -16,17 +16,27 @@ function Header({IsBgGray, IsTextWhite, NavBgLight, DarkText, NavBgDark, LightTe
                     
 
                     {/* Navigation */}
-                    <nav className="hidden md:flex md:flex-grow">
+                    <nav className="hidden md:flex md:flex-grow flex w-full">
 
                         <ul className="flex flex-wrap items-center flex-grow">
                             <li>
-                                <Link to="/about" className={`hover:bg-gray-200 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
+                                <Link to="/about" className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
                                     About
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/features" className={`hover:bg-gray-200 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${IsBgGray && "bg-gray-900"} ${IsTextWhite && "text-white"}`}>
+                                <Link to="/features" className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
                                     Features
+                                </Link>
+                            </li>
+                            <li>
+                               <Link to="/projects"className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
+                                  Projects
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/contact"className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
+                                   Contact
                                 </Link>
                             </li>
                         </ul>
