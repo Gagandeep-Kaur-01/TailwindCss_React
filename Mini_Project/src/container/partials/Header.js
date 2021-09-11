0baddd033d42dfafa5740248b805af1ba72673a0
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-09-11 16:11:01 +0530

    updated css

--- Mini_Project/src/container/partials/Header.js
+++ Mini_Project/src/container/partials/Header.js
@@ -18,28 +18,46 @@ function Header({IsBgGray, IsTextWhite, NavBgLight, DarkText, NavBgDark, LightTe
                     {/* Navigation */}
                     <nav className="hidden md:flex md:flex-grow flex w-full">
 
-                        <ul className="flex flex-wrap items-center flex-grow">
+                        <ul className="flex items-center flex-grow">
                             <li>
-                                <Link to="/about" className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
+                                <Link to="/about" className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out font-bold ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
                                     About
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/features" className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
+                                <Link to="/features" className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out font-bold ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
                                     Features
                                 </Link>
                             </li>
                             <li>
-                               <Link to="/projects"className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
+                               <Link to="/projects"className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out font-bold ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
                                   Projects
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/contact"className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
+                                <Link to="/contact"className={`hover:bg-purple-500 hover:text-black px-6 py-2 flex items-center transistion duration-150 ease-in-out font-bold ${NavBgLight && "bg-gray-200"} ${DarkText && "text-black"} ${NavBgDark && "bg-gray-800"} ${LightText && "text-white"}`}>
                                    Contact
                                 </Link>
                             </li>
                         </ul>
+
+                        {/* Sign in links */}
+                        <ul className="flex flex-grow justify-end items-center ml-50">
+              
+                            <li>
+                                <Link to="/signin" className="text-gray-300 hover:text-gray-200 ml-50 pr-5 py-2 flex items-center transition duration-150 ease-in-out font-bold ">
+                                    Login
+                                </Link>
+                                 
+                            </li>
+                            <li>
+                                <Link to="/signup" className="inline-flex justify-center py-1 px-3 border border-transparent shadow-sm text-sm  font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 font-bold">
+                                    Sign Up
+                                </Link>
+                            </li>
+
+                        </ul>
+
                     </nav>
 
                 </div>
