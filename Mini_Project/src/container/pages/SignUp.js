2cc62818f0f5e41b73252007ce0d1f8011b49bf3
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-09-11 17:08:41 +0530

    inserted Signup page structure

---
+++ Mini_Project/src/container/pages/SignUp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../partials/Header';
+
+export default function SignUp() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden bg-gray-900">
+
+      {/* Header */}
+      <Header NavBgLight={true} DarkText={true} />
+
+    
+
+    </div>
+  );
+}
